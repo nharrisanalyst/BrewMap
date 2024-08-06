80e73fbd3a32b1296d98939108c86ceeb55da6df
nathan harris <nharrisanalyst@gmail.com>
2024-08-06 15:12:35 -0700

    added story and finished <Price />

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -11,10 +11,10 @@ type PriceProps = {
 export function Price({menuItem, price, reporter='BrewMapData'}:PriceProps){
 
     return(
-           <div className="flex">
-            <Money price={price} />
-            <div>{menuItem}</div>
-            <div>{reporter}</div>
+           <div className="flex flex-col items-center ">
+            <div className='*:-ml-2 *:leading-5'><Money price={price} /></div>
+            <div className='font-normal text-smx leading-3 text-slate-700'>{menuItem}</div>
+            <div className='font-extralight text-xs leading-4 text-slate-400'>{reporter}</div>
 
 
            </div>
